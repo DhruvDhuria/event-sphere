22fdd11a159b23ca92a9af56fc93fdd2ba5278bb
Dhruv Dhuria <dhuriadhruv13@gmail.com>
2025-03-28 17:15:07 +0530

    Optimize event fetching using mongodb aggregation pipeline
    - Replace single find query with aggregation to fetch event details
    - Include category and organizer details in a single database query
    - Improve data retrieval efficiency

--- src/app/api/events/[eventId]/route.ts
+++ src/app/api/events/[eventId]/route.ts
@@ -1,13 +1,57 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Event from "@/models/Event";
 import dbConnect from "@/lib/dbConnect";
+import mongoose from "mongoose";
+
 
 dbConnect();
 
-export async function GET( {params}: {params: {eventId: string}}) {
+export async function GET(req: NextRequest, {params}: {params: {eventId: string}}) {
     // get event details based on event id
     try {
-        const event = await Event.findById(params.eventId);
+        const {eventId} = await params
+        console.log(eventId)
+        const event = await Event.aggregate([
+            {$match: {_id: new mongoose.Types.ObjectId(eventId)}},
+          {
+            $lookup: {
+              from: "users",
+              localField: "organizerId",
+              foreignField: "_id",
+              as: "organizer",
+            },
+          },
+          {
+            $lookup: {
+              from: "categories",
+              localField: "category",
+              foreignField: "_id",
+              as: "category",
+            },
+          },
+          {
+            $addFields: {
+              organizer: { $arrayElemAt: ["$organizer", 0] },
+              category: { $arrayElemAt: ["$category", 0] },
+            },
+          },
+          {
+            $project: {
+              "organizer.username": 1,
+              "organizer.email": 1,
+              "category.name": 1,
+              title: 1,
+              description: 1,
+              date: 1,
+              time: 1,
+              location: 1,
+              image: 1,
+              createdAt: 1,
+              updatedAt: 1,
+            },
+          },
+        ]);
+        
 
         if (!event) {
             return NextResponse.json({ error: "Event not found" }, { status: 404 });
