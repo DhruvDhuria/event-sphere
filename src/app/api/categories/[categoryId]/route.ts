bd3a51cb4a5217c049ae840672d6dd51272a9bf9
Dhruv Dhuria <dhuriadhruv13@gmail.com>
2025-03-31 21:17:14 +0530

    fix build error

--- src/app/api/categories/[categoryId]/route.ts
+++ src/app/api/categories/[categoryId]/route.ts
@@ -7,7 +7,7 @@ dbConnect();
 
 export async function GET(req: NextRequest, { params }: { params: { categoryId: string } }) {
     try {
-        const { categoryId } = await params;
+        const { categoryId } = params;
         const category = await Category.findById(categoryId)
         if (!category){
             return NextResponse.json({error: "cannot find category"}, {status: 404})
